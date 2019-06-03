acf3f99807a994da62c8ca0ce05689724e32f7b0
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-06-03 10:03:37 +0200

    avoid using `.` in every step

--- esm/index.js
+++ esm/index.js
@@ -11,6 +11,8 @@ const {
   hasOwnProperty
 } = Object;
 
+const {concat, forEach, includes, push} = [];
+
 const falsify = (descriptor, name) => {
   defineProperty(descriptor, name, {
     enumerable: true,
@@ -30,16 +32,23 @@ export default object => {
   const names = [];
   const descriptors = [];
   do {
-    getOwnPropertyNames(object).concat(getOwnPropertySymbols(object))
-    .forEach(name => {
-      if (!names.includes(name)) {
-        names.push(name);
-        descriptors.push(getOwnPropertyDescriptor(object, name));
+    call(
+      forEach,
+      call(
+        concat,
+        getOwnPropertyNames(object),
+        getOwnPropertySymbols(object)
+      ),
+      name => {
+        if (!call(includes, names, name)) {
+          call(push, names, name);
+          call(push, descriptors, getOwnPropertyDescriptor(object, name));
+        }
       }
-    });
+    );
   }
   while (object = getPrototypeOf(object));
-  names.forEach((name, i) => {
+  call(forEach, names, (name, i) => {
     defineProperty(self, name, updated(descriptors[i]));
   });
   return self;
